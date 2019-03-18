8a2042fcf1886aabfee7015e31b73a7abdce4799
Farzad Sharif Bakhtiar <fsharifb@sfu.ca>
2019-03-18 11:17:11 -0700

    updated job script. Job names MUST be set from now on.

--- job.sh
+++ job.sh
@@ -3,6 +3,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=4000
 #SBATCH --time=0-01:30
+#SBATCH --output=%x.out
+# %x is the job name
+
+JOB_NAME=${SBATCH_JOB_NAME}
 source startup.sh
-python model.py -e ${NUM_EPOCHS:-50} -d '../datasets'  ${RUN_FLAGS:-''}
+python model.py -e ${NUM_EPOCHS:-50} -d '../datasets' -m ${JOB_NAME}.model  ${RUN_FLAGS:-''}
 
