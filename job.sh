9536aeb08e22fab88717200de093945b55184cc9
Farzad Sharif Bakhtiar <fsharifb@sfu.ca>
2019-03-19 09:00:09 -0700

    job script

--- job.sh
+++ job.sh
@@ -4,8 +4,10 @@
 #SBATCH --mem=4000
 #SBATCH --time=0-01:30
 #SBATCH --output=%x.out
+#SBATCH --mail-user=fsharifb@sfu.ca
+#SBATCH --mail-type=ALL
 # %x is the job name
 
 source startup.sh
-python model.py -e ${NUM_EPOCHS:-50} -d '../datasets' -a -m ${SLURM_JOB_NAME}.model  -l ${MODEL_NAME:-'outputs/regular.model'} {RUN_FLAGS:-''}
+python model.py -e ${NUM_EPOCHS:-50} -d '../datasets' -a -m ${SLURM_JOB_NAME}.model  -l ${MODEL_NAME:-'outputs/regular.model'} ${RUN_FLAGS:-''}
 
