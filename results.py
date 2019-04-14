c65bc0e8dcce7ef0631a30980069aefd23138ca2
Farzad Sharif Bakhtiar <fsharifb@sfu.ca>
2019-04-14 14:52:18 -0700

    results works

--- results.py
+++ results.py
@@ -2,24 +2,26 @@ import outputparse
 
 class Results:
 	OUTPUT_FOLDERS=[
+			('experiment_outputs/feat_norm_outputs/2/', 'acc'),
 			('outputs/full/vanilla/', 'acc'),
-			('full_diff_base_net_outputs', 'acc'),
+			('full_diff_base_nets_outputs/', 'acc'),
 	]
 
 	def __init__(self, sort='valid_acc'):
 		self.p = outputparse.Parser(sort_column=sort)
 
-	def top_10(self):
-		return self.top_k(10)
+	def top_10(self, table=False):
+		return self.top_k(10, table=table)
 
-	def top_k(self, k):
+	def top_k(self, k, table=False):
 		results = self.p.parse_folders([folder for folder, mode in type(self).OUTPUT_FOLDERS], sort=True)
-		return results[len(results) - k:]
+		top_k = results[len(results) - k:]
+		return self.p.tabulate(top_k) if table else top_k
 
 
 def run():
 	r = Results()
-	r.top_10()
+	print(r.top_10(table=True))
 
 if __name__ == '__main__':
 	run()
