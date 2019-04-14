143ddf018c0d470eec78c4293d16bd9400feed20
Farzad Sharif Bakhtiar <fsharifb@sfu.ca>
2019-04-14 14:17:41 -0700

    modified regular job to get an average

--- jobs/regular_job.sh
+++ jobs/regular_job.sh
@@ -3,11 +3,20 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16000
 #SBATCH --time=0-01:30
-#SBATCH --output=%x.out
+#SBATCH --output=temp/regular_%a.out
 #SBATCH --mail-user=fsharifb@sfu.ca
 #SBATCH --mail-type=ALL
+#SBATCH --array=0-19
 # %x is the job name
 
+as=('' '-a')
+ats('out' '')
+jn=${SLURM_JOB_NUM}
+let ai=$jn%2
+a=${as[$ai]}
+at=${ats[$ai]}
+output_dir=averages/regular/with${at}_augment/
+mkdir -p $output_dir
 source startup.sh
-python model.py -e 120 -d '../datasets' -a -m regular.model
+python model.py -e 120 -d '../datasets' ${a} -m ${output_dir}/regular_${ai}.model > ${output_dir}/regular_${ai}.out
 
