4e766dd5c40a0cce084b5a09c286318813e08e6a
Farzad Sharif Bakhtiar <farzadsbakhtiar@gmail.com>
2019-04-14 17:20:16 -0400

    modified regular job to request the right resources

--- jobs/regular_job.sh
+++ jobs/regular_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-functor
 #SBATCH --gres=gpu:1
-#SBATCH --mem=16000
-#SBATCH --time=0-01:30
+#SBATCH --mem=8G
+#SBATCH --time=0-01:00
 #SBATCH --output=temp/regular_%a.out
 #SBATCH --mail-user=fsharifb@sfu.ca
 #SBATCH --mail-type=ALL
@@ -16,6 +16,7 @@ let ai=$jn%2
 a=${as[$ai]}
 at=${ats[$ai]}
 output_dir=averages/regular/with${at}_augment/
+mkdir -p temp
 mkdir -p $output_dir
 source startup.sh
 python model.py -e 120 -d '../datasets' ${a} -m ${output_dir}/regular_${ai}.model > ${output_dir}/regular_${ai}.out
